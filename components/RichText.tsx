2cec23473171641781c635d8822e9a7aaf9958e8
jdjfisher <43887886+jdjfisher@users.noreply.github.com>
2023-03-29 21:26:00 +0100

    Render RichText assets as next images

--- components/RichText.tsx
+++ components/RichText.tsx
@@ -1,8 +1,22 @@
 import { documentToReactComponents, Options } from '@contentful/rich-text-react-renderer';
-import { Document } from '@contentful/rich-text-types';
+import { Document, BLOCKS } from '@contentful/rich-text-types';
+import Image from 'next/image';
 
 const options: Options = {
-  //
+  renderNode: {
+    [BLOCKS.EMBEDDED_ASSET]: (node) => {
+      const file = node.data.target.fields.file;
+
+      return (
+        <Image
+          src={`https://${file.url}`}
+          height={file.details.image.height}
+          width={file.details.image.width}
+          alt={node.data.target.fields.description}
+        />
+      );
+    },
+  },
 };
 
 interface Props {
