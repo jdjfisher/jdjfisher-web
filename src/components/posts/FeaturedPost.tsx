23ec60ce9cbad39885ba170cf5436ddb6d5c21aa
jdjfisher <43887886+jdjfisher@users.noreply.github.com>
2023-09-02 09:58:13 +0100

    Swap Next.js for Astro

---
+++ src/components/posts/FeaturedPost.tsx
@@ -0,0 +1,17 @@
+import type { Post } from '../../types';
+import dayjs from 'dayjs';
+import RowLink from '../layout/RowLink';
+
+interface Props {
+  post: Post;
+}
+
+export default function FeaturedPost({ post }: Props) {
+  return (
+    <RowLink href={`/posts/${post.slug}`}>
+      <span className="text-sm text-gray-500">{dayjs(post.postedAt).format('MMMM Do, YYYY')}</span>
+      <h2 className="text-lg">{post.title}</h2>
+      <p className="text-gray-500"> {post.description}</p>
+    </RowLink>
+  );
+}
---
+++ src/lib/cms/client.ts
@@ -0,0 +1,13 @@
+import contentful from 'contentful';
+
+const space = import.meta.env.CONTENTFUL_SPACE_ID;
+const accessToken = import.meta.env.CONTENTFUL_ACCESS_TOKEN;
+
+if (!space || !accessToken) {
+  throw 'Missing CMS environment configuration';
+}
+
+export const cmsClient = contentful.createClient({
+  space,
+  accessToken,
+});
