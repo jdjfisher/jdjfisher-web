42be9085dab9ad5b410cbd3ce588ef06d5807ce9
jdjfisher <43887886+jdjfisher@users.noreply.github.com>
2025-05-30 21:58:08 +0100

    Updated dependencies & tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import tailwindcss from '@tailwindcss/vite';
 
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({ applyBaseStyles: false }), react()],
-  image: {
-    domains: ['images.ctfassets.net'],
-  },
+  integrations: [react()],
+  image: { domains: ['images.ctfassets.net'] },
+  vite: { plugins: [tailwindcss()] },
 });
