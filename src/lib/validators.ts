904c5c4bcd538a46cb201a4daece8a92d760b617
jdjfisher <43887886+jdjfisher@users.noreply.github.com>
2025-07-04 14:13:26 +0100

    Replace rich text with marked

--- src/lib/validators.ts
+++ src/lib/validators.ts
@@ -1,11 +1,5 @@
 import { z } from 'zod';
 
-export const richTextValidator = z.object({
-  nodeType: z.literal('document'),
-  content: z.array(z.any()),
-  data: z.record(z.string(), z.any()),
-});
-
 export const collaboratorValidator = z.object({
   handle: z.string(),
   referenceUrl: z.string().url(),
@@ -20,29 +14,5 @@ export const projectValidator = z.object({
   status: z.enum(['closed', 'paused', 'prototyping', 'active']),
   collaborators: z.array(collaboratorValidator).optional(),
   order: z.number().int(),
-  description: richTextValidator,
+  description: z.string(),
 });
-
-export const wordsValidator = z
-  .object({
-    fields: z.object({
-      content: richTextValidator,
-    }),
-  })
-  .transform((o) => o.fields);
-
-export const assetValidator = z
-  .object({
-    fields: z.object({
-      title: z.string(),
-      description: z.string(),
-      file: z.object({
-        fileName: z.string(),
-        url: z.string(),
-        details: z.object({
-          size: z.number().positive(),
-        }),
-      }),
-    }),
-  })
-  .transform((o) => o.fields);
